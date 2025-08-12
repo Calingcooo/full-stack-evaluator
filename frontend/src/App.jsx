d24df697b4c716bbca4e3aba12b2669905093eb5
Calingcooo <calingco.je@gmail.com>
2025-08-12 11:00:38 +0800

    install tailwind css for styling

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,8 @@
-import './App.css'
 import Tasks from "./Tasks"
 
 function App() {
   return (
-    <div className="app">
+    <div className="flex flex-col">
       <h1>📝 React Task Evaluator</h1>
       <Tasks />
     </div>
